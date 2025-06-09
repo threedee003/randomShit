ef4680c7b17524442c8da89a8298cb878008aa5d
Tribikram Dhar <74320520+threedee003@users.noreply.github.com>
2025-06-09 17:53:00 +0530

    Update pick.py

--- iiwa_workspace/pick.py
+++ iiwa_workspace/pick.py
@@ -1,6 +1,6 @@
 
-from iiwa_ws import iiwaScene
-
+from python.iiwa_workspace.iiwa_ws import iiwaScene
+from isaacgym import gymapi
 import numpy as np
 
 class Pick(iiwaScene):
@@ -9,11 +9,40 @@ class Pick(iiwaScene):
                  ):
         self.scale_reward = scale_reward
         pass
+    
+    #NOTE: To be used only if velocity control is used.
+    def reach_jt_position(self, desired_jt: np.ndarray):
+        kp = 6.
+        kd = 1.
+        assert self.control_type == 'velocity', f"only for velocity control"
+        dof_states = self.gym.get_actor_dof_states(self.env, self.robot_handle, gymapi.STATE_ALL)
+        joint_pos = [dof_state['pos'] for dof_state in dof_states]
+        joint_vel = [dof_state['vel'] for dof_state in dof_states]
+        jt_error = desired_jt-np.array(joint_pos)
+        vel_cmd = (kp*jt_error - kd*np.array(joint_vel)).tolist()
+        self.gym.set_actor_dof_velocity_targets(self.env, self.robot_handle, vel_cmd)
 
-    def apply_action(self, action):
-        pass
 
-    def compute_rewards(self):
+
+    def apply_arm_action(self, action: np.ndarray):
+        if self.control_type == 'position':
+            dof_states = self.gym.get_actor_dof_states(self.env, self.robot_handle, gymapi.STATE_ALL)
+            joint_pos = [dof_state['pos'] for dof_state in dof_states]
+            gripper_jt1 = joint_pos[7]
+            gripper_jt2 = joint_pos[8]
+            action[7] = gripper_jt1
+            action[8] = gripper_jt2
+            self.gym.set_actor_dof_position_targets(self.env, self.robot_handle, action.tolist())
+        elif self.control_type == 'velocity':
+            pass
+
+        else:
+            raise NotImplementedError("I will not implement it.")
+        
+    
+    def gripper_action(self, open: bool)
+
+    def compute_reward(self):
         x = None
         y = None
         reward = 0.
